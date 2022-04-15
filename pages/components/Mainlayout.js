d0c08e65153a4631428f0f2ea9e26d548fd005c4
kimjay1h12 <olawaleadeit@gmail.com>
2022-04-14 20:19:25 -0700

    update

--- pages/components/Mainlayout.js
+++ pages/components/Mainlayout.js
@@ -2,10 +2,22 @@ import { Typography } from "@mui/material";
 import Head from "next/head";
 import Toolbar from "../Toolbar";
 function Mainlayout() {
+  const title = "Foodwest"
   return (
     <div>
       <Head> 
-        <title>Foodwest</title>
+        
+       
+        <title> {title} | Foodwest</title>
+        <meta property="og:title" content={title} />
+        <meta
+          property="og:description"
+          content={
+            description ||
+            "welcome to the best food ordering website "
+          }
+        />
+    
         <meta name="description" content="Developed by kimjay" />
       </Head>
       <Toolbar />
