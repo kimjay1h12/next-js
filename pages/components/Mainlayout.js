3e920b1674e7049f6f7f9460eae5b9523905dc47
kimjay1h12 <olawaleadeit@gmail.com>
2022-04-14 21:35:34 -0700

    update

--- pages/components/Mainlayout.js
+++ pages/components/Mainlayout.js
@@ -1,33 +1,28 @@
 import { Typography } from "@mui/material";
 import Head from "next/head";
 import Toolbar from "./Toolbar";
-function Mainlayout( ) {
-  
+function Mainlayout() {
   return (
     <div>
-           <Head>
-        <title>
-          Foodwest
-        </title>
+      <Head>
+        <meta
+          property="og:image"
+          content="https://img.resized.co/98fm/eyJkYXRhIjoie1widXJsXCI6XCJodHRwczpcXFwvXFxcL21lZGlhLnJhZGlvY21zLm5ldFxcXC91cGxvYWRzXFxcLzIwMTlcXFwvMDlcXFwvMjUxMzM2MTBcXFwvaGFtYnVyZ2VyLmpwZ1wiLFwid2lkdGhcIjo5NzAsXCJoZWlnaHRcIjo0ODUsXCJkZWZhdWx0XCI6XCJodHRwczpcXFwvXFxcL3d3dy45OGZtLmNvbVxcXC9pbWFnZXNcXFwvbm9faW1hZ2UucG5nXCJ9IiwiaGFzaCI6ImMxMDE1NWM5MmU4NTVhNjc1NzllNzZiYjNlZmQzMmFhZmYyODIwNjAifQ==/blonde-moments-i-thought-a-hamburger-was-called-a-handburger-for-years.jpg"
+        />
+        <title>Foodwest</title>
         <meta property="og:url" content="foodwest.vercel.app" />
         <meta property="og:type" content="Foodwest" />
         <meta property="fb:app_id" content="Foodwest" />
-        <meta
-          property="og:title"
-          content="Welcome to Foodwest"
-          
-        />
+        <meta property="og:title" content="Welcome to Foodwest" />
         <meta name="twitter:card" content="summary" />
-        <meta
-          property="og:description"
-          content="Welcome to Foodwest"
-        />
-         <link rel="shortcut icon" href="/img/pizza.jpg"/>
-        <meta property="og:image" content="/img/cheese.jpg" />
+        <meta property="og:description" content="Welcome to Foodwest" />
+
+        <meta property="og:image:width" content="1200" />
+
+        <meta property="og:image:height" content="630" />
       </Head>
-    
+
       <Toolbar />
-     
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Header from "./components/Header";
 import Mainlayout from "./components/Mainlayout";
 import Section from './components/Section'
-import Head from "next/head";
+
 import CssBaseline from "@mui/material/CssBaseline";
 export default function Home(){
   return(
